d02c24a682d1e80e9e645000381631536c99f519
Erfan <erfankm7@gmail.com>
2023-06-03 11:12:59 +0330

    first commit

---
+++ lib/persian_vehicle_pelak.dart
@@ -0,0 +1,17 @@
+library persian_vehicle_pelak;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PersianVehiclePelak extends StatelessWidget {
+  const PersianVehiclePelak({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.amber,
+    );
+  }
+}

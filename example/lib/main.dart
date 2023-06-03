dc9b7e85bfe58ced923ad0cf41e2c36da11bae6c
Erfan <erfankm7@gmail.com>
2023-06-03 18:12:42 +0330

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,55 +25,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Directionality(
-      textDirection: TextDirection.rtl,
-      child: Container(
-        width: 200,
-        height: 70,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(4),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Text('ایران'), Text('20')],
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('262'),
-                  Text('ب'),
-                  Text('22'),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue, border: Border.all(color: Colors.black)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(child: Text('🇮🇷')),
-                  Text('IR',
-                      style: TextStyle(fontSize: 10, color: Colors.white)),
-                  Text(
-                    'IRAN',
-                    style: TextStyle(fontSize: 10, color: Colors.white),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+      body: Center(child: PersianVehiclePelak()),
+    );
   }
 }
--- lib/persian_vehicle_pelak.dart
+++ lib/persian_vehicle_pelak.dart
@@ -2,42 +2,91 @@ library persian_vehicle_pelak;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PersianVehiclePelak extends StatelessWidget {
   double? width = 200;
   double? height = 100;
 
-  PersianVehiclePelak({this.width, this.height});
+  PersianVehiclePelak({super.key, this.width, this.height});
+
+  final Widget iranSVG = SvgPicture.asset(
+      'packages/persian_vehicle_pelak/assets/images/iran-light.svg');
+
+  final Widget flagVG = SvgPicture.asset(
+      'packages/persian_vehicle_pelak/assets/images/iran-flag.svg');
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Container(
-        width: width,
-        height: height,
+        width: 200,
+        height: 40,
         decoration: BoxDecoration(
-          color: Colors.blueAccent,
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(16),
-        ),
+            color: Colors.amber,
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(6)),
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: [
-                Text('ایران'),
-                Expanded(
-                  child: Text('20'),
-                )
-              ],
+            Container(
+              decoration: BoxDecoration(
+                border: Border.fromBorderSide(
+                  BorderSide(color: Colors.black),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 2), child: iranSVG),
+                  const Expanded(
+                      child: Center(
+                          child: Text(
+                    '10',
+                    style: TextStyle(fontSize: 24),
+                  )))
+                ],
+              ),
             ),
-            Text('262'),
-            Text('ب'),
-            Text('22'),
-            Column(
-              children: [Text('🇮🇷'), Text('IR'), Text('IRAN')],
+            Expanded(
+              child: Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    '222',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'سلام چطوری ؟',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontFamily:
+                            'packages/persian_vehicle_pelak/assets/fonts/Regular.ttf'),
+                  ),
+                  Text(
+                    '23',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontFamily:
+                            'packages/persian_vehicle_pelak/assets/fonts/Bold.ttf'),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Color.fromARGB(255, 0, 52, 154)),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 2), child: flagVG),
+                ],
+              ),
             )
           ],
         ),

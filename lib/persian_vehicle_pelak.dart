dc8e29bf3dbe68ac466545867b25fa7e4fe417b9
Erfan <erfankm7@gmail.com>
2023-06-03 13:28:07 +0330

    Infrastructure

--- lib/persian_vehicle_pelak.dart
+++ lib/persian_vehicle_pelak.dart
@@ -4,14 +4,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PersianVehiclePelak extends StatelessWidget {
-  const PersianVehiclePelak({super.key});
+  double? width = 200;
+  double? height = 100;
+
+  PersianVehiclePelak({this.width, this.height});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      color: Colors.amber,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                Text('ایران'),
+                Expanded(
+                  child: Text('20'),
+                )
+              ],
+            ),
+            Text('262'),
+            Text('ب'),
+            Text('22'),
+            Column(
+              children: [Text('🇮🇷'), Text('IR'), Text('IRAN')],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
